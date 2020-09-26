a5f952ab7b8a500be38d5f73681973c1acc5d4df
Anthony Joseph <ajosephau@users.noreply.github.com>
2020-09-26 12:55:34 +1000

    Updates

---
+++ precache-manifest.3bfa20192d5c7f63593037098c4ee7e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5828975b50659bacc13dc3808ce52e2b",
+    "url": "/react-apollo-spacex/index.html"
+  },
+  {
+    "revision": "eac1d0037e52d45f5c41",
+    "url": "/react-apollo-spacex/static/js/2.cee73cfc.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/react-apollo-spacex/static/js/2.cee73cfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2b1eed983709ee9d9b8",
+    "url": "/react-apollo-spacex/static/js/main.ac8e68ef.chunk.js"
+  },
+  {
+    "revision": "1b37e4d0f23c9ee2c98c",
+    "url": "/react-apollo-spacex/static/js/runtime-main.41c0ad0e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-apollo-spacex/precache-manifest.70e9650d3aa40c6f156578d0d1353a9f.js"
+  "/react-apollo-spacex/precache-manifest.3bfa20192d5c7f63593037098c4ee7e9.js"
 );
 
 self.addEventListener('message', (event) => {
